8f8349ba6279c95ba9f8d73fe5fafba802ca8000
Enio Gonzalez <gonzalezeniood@gmail.com>
2021-03-28 23:51:34 -0300

    se agrego un template de html para hacer ejemplos y probar ciertas cosas

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 4500;
 
-http.createServer((req, res) => {
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', function (req, res) {
+    res.send('Hola Mundo')
+})
 
-    console.log(req);
+app.post('/', function (req, res) {
+    res.send('Post del path /')
+})
 
-    //res.writeHead(200, {'Content-Type': 'application/json'});
+app.get('/usuario', function (req, res) {
+    res.send('Usuario Enio')
+})
 
-    /*const user = {
-        id: 123,
-        name: 'Enio'
-    }
-    res.write(JSON.stringify(user));*/
-
-    res.setHeader('Conten-Disposition', 'attachment; filename=usuarios.csv');
-    res.writeHead(200, {'Content-Type': 'application/csv'});
-    res.write('id,name\n123,Enio\n456, Luisa');
-
-
-
-    res.end();
-
-
-}).listen(4000);
+app.listen(port, () => {
+    console.log(`Server started - Port ${port}`);
+});
